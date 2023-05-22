7548b72643b8f2874d6de370b1a36fbb89f6b2ab
Lautaro <lautarofrancomagro@gmail.com>
2023-05-22 10:28:11 -0300

    funcion_1

---
+++ funcion_1.py
@@ -0,0 +1,14 @@
+import re
+
+# Crear una función llamada aplicarAumento que reciba como parámetro el precio de un producto y devuelva el valor del producto con un aumento del 5%. Realizar la llamada desde el main.
+
+def aplicar_aumento(precio:float) -> int:
+    patron = r"^\d"
+    retorno = 0.0
+    if re.match(patron, precio):
+        retorno = float(precio) * 1.05
+        return round(retorno,2)
+    else:
+        return "Lo ingresado no es un numero"
+
+print(aplicar_aumento("4819.41"))
